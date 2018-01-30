943497ae6276676635adbaf7706845d528731f7f
Alejandro Frías <alejandrofdiaz@gmail.com>
2018-01-30 23:14:56 +0100

    Remove Test from gulp bundle build

--- gulpfile.js
+++ gulpfile.js
@@ -6,18 +6,18 @@ const babel = require('gulp-babel');
 const sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('default', function() {
-    const result = gulp.src('src/*.ts').pipe(tsProject());
+  const result = gulp.src(['src/**/*.ts', '!src/**/__tests__/*.ts']).pipe(tsProject());
 
-    const resultJs = result.js
-        .pipe(sourcemaps.init())
-        .pipe(
-            babel({
-                presets: ['babel-preset-env', 'babel-preset-minify'],
-                plugins: ['transform-remove-console','babel-plugin-lodash']
-            })
-        )
-        .pipe(sourcemaps.write(''));
+  const resultJs = result.js
+    .pipe(sourcemaps.init())
+    .pipe(
+      babel({
+        presets: ['babel-preset-env', 'babel-preset-minify'],
+        plugins: ['transform-remove-console', 'babel-plugin-lodash']
+      })
+    )
+    .pipe(sourcemaps.write(''));
 
-    const resultDts = result.dts;
-    return merge([resultJs.pipe(gulp.dest('build')), resultDts.pipe(gulp.dest('build'))]);
+  const resultDts = result.dts;
+  return merge([resultJs.pipe(gulp.dest('build')), resultDts.pipe(gulp.dest('build'))]);
 });
