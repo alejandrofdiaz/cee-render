184eb942ff4633865b3c6d8650dde0d8d5c1eae5
Alejandro Frías <alejandrofdiaz@gmail.com>
2018-02-06 22:29:38 +0100

    Simplified xml getting data

--- src/parser.ts
+++ src/parser.ts
@@ -9,40 +9,39 @@ import { SummaryCertificado } from './model';
  * @returns {SummaryCertificado}
  */
 function parseDataFromSummary(xml: string) {
-  let _xmlFullTree = xml2js(xml);
-  const _xmlDataTree = _xmlFullTree.elements ? _xmlFullTree.elements[0] : null;
+  let _xmlFullTree = xml2js(xml, {
+    compact: true,
+    trim: true,
+    ignoreDeclaration: true,
+    ignoreDoctype: true,
+    ignoreAttributes: true
+  });
+  const _xmlDataTree = _xmlFullTree.DatosEnergeticosDelEdificio
+    ? _xmlFullTree.DatosEnergeticosDelEdificio
+    : null;
   const summary = new SummaryCertificado();
 
-  if (_xmlDataTree && _xmlDataTree.name === 'DatosEnergeticosDelEdificio') {
+  if (!!_xmlDataTree) {
     try {
-      summary.anio = _xmlDataTree.elements
-        .find(item => item.name === 'IdentificacionEdificio')
-        .elements.find(item => item.name === 'AnoConstruccion').elements[0].text;
+      summary.anio = _xmlDataTree.IdentificacionEdificio.AnoConstruccion._text;
     } catch (err) {
       console.log('error anio');
     }
 
     try {
-      summary.normativa = _xmlDataTree.elements
-        .find(item => item.name === 'IdentificacionEdificio')
-        .elements.find(item => item.name === 'NormativaVigente').elements[0].text;
+      summary.normativa = _xmlDataTree.IdentificacionEdificio.NormativaVigente._text;
     } catch (err) {
       console.log('error normativa');
     }
 
     try {
-      summary.referenciaCatastral = _xmlDataTree.elements
-        .find(item => item.name === 'IdentificacionEdificio')
-        .elements.find(item => item.name === 'ReferenciaCatastral').elements[0].text;
+      summary.referenciaCatastral = _xmlDataTree.IdentificacionEdificio.ReferenciaCatastral._text;
     } catch (err) {
       console.log('error referenciaCatastral');
     }
 
     try {
-      summary.tipoEdificio = _xmlDataTree.elements
-        .find(item => item.name === 'IdentificacionEdificio')
-        .elements.find(item => item.name === 'TipoDeEdificio').elements[0].text;
-
+      summary.tipoEdificio = _xmlDataTree.IdentificacionEdificio.TipoDeEdificio._text;
       summary.tipoEdificio = summary.tipoEdificio
         .replace(/([A-Z])/g, ' $1')
         .replace(/^./, str => str.toUpperCase());
@@ -51,77 +50,57 @@ function parseDataFromSummary(xml: string) {
     }
 
     try {
-      summary.direccion = _xmlDataTree.elements
-        .find(item => item.name === 'IdentificacionEdificio')
-        .elements.find(item => item.name === 'Direccion').elements[0].text;
+      summary.direccion = _xmlDataTree.IdentificacionEdificio.Direccion._text;
     } catch (err) {
       console.log('error direccion');
     }
 
     try {
-      summary.municipio = _xmlDataTree.elements
-        .find(item => item.name === 'IdentificacionEdificio')
-        .elements.find(item => item.name === 'Municipio').elements[0].text;
+      summary.municipio = _xmlDataTree.IdentificacionEdificio.Municipio._text;
     } catch (err) {
       console.log('error municipio');
     }
 
     try {
-      summary.cp = _xmlDataTree.elements
-        .find(item => item.name === 'IdentificacionEdificio')
-        .elements.find(item => item.name === 'CodigoPostal').elements[0].text;
+      summary.cp = _xmlDataTree.IdentificacionEdificio.CodigoPostal._text;
     } catch (err) {
       console.log('error cp');
     }
+
     try {
-      summary.comunidadAutonoma = _xmlDataTree.elements
-        .find(item => item.name === 'IdentificacionEdificio')
-        .elements.find(item => item.name === 'ComunidadAutonoma').elements[0].text;
+      summary.comunidadAutonoma = _xmlDataTree.IdentificacionEdificio.ComunidadAutonoma._text;
     } catch (err) {
       console.log('error comunidadAutonoma');
     }
+
     try {
-      summary.fecha = _xmlDataTree.elements
-        .find(item => item.name === 'DatosDelCertificador')
-        .elements.find(item => item.name === 'Fecha').elements[0].text;
+      summary.fecha = _xmlDataTree.DatosDelCertificador.Fecha._text;
     } catch (err) {
       console.log('error fecha');
     }
 
     try {
-      summary.consumoEnergia = _xmlDataTree.elements
-        .find(item => item.name === 'Consumo')
-        .elements.find(item => item.name === 'EnergiaPrimariaNoRenovable')
-        .elements.find(item => item.name === 'Global').elements[0].text;
-
+      summary.consumoEnergia = _xmlDataTree.Consumo.EnergiaPrimariaNoRenovable.Global._text;
       summary.consumoEnergia = String(Math.floor(Number(summary.consumoEnergia)));
     } catch (err) {
       console.log('error consumoEnergia');
     }
 
     try {
-      summary.calificacionConsumoEnergia = _xmlDataTree.elements
-        .find(item => item.name === 'Calificacion')
-        .elements.find(item => item.name === 'EnergiaPrimariaNoRenovable')
-        .elements.find(item => item.name === 'Global').elements[0].text;
+      summary.calificacionConsumoEnergia =
+        _xmlDataTree.Calificacion.EnergiaPrimariaNoRenovable.Global._text;
     } catch (err) {
       console.log('error letraEnergia');
     }
 
     try {
-      summary.emisionesCO2 = _xmlDataTree.elements
-        .find(item => item.name === 'EmisionesCO2')
-        .elements.find(item => item.name === 'Global').elements[0].text;
-      summary.emisionesCO2 = String(Math.floor(Number(summary.emisionesCO2)));
+      summary.emisionesCO2 = _xmlDataTree.EmisionesCO2.Global._text;
     } catch (err) {
       console.log('error emisionesCO2');
     }
 
     try {
-      summary.calificacionEmisiones = _xmlDataTree.elements
-        .find(item => item.name === 'Calificacion')
-        .elements.find(item => item.name === 'EmisionesCO2')
-        .elements.find(item => item.name === 'Global').elements[0].text;
+      summary.calificacionEmisiones = _xmlDataTree.Calificacion.EmisionesCO2.Global._text;
     } catch (err) {
       console.log('error letra consumo energia');
     }
@@ -129,6 +108,7 @@ function parseDataFromSummary(xml: string) {
     summary.generateResumen();
     summary.setCalificacionSelectableValues();
   }
+
   return summary;
 }
 
